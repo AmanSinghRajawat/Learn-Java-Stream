c5c7899f6af0ac733347de4ce1832cb3e7fe7215
AmanSinghRajawat <amansinghrajawat17@gmail.com>
2024-06-18 11:32:56 +0530

    Adding Q3

---
+++ InterviewQuestions/Q3.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q3 {
+    public static void main(String[] args) {
+
+        String arr[] = { "zean", "rahul", "vicky", "sameer", "aman" };
+
+        System.out.println("Before Sorting : "+Arrays.toString(arr));
+
+        // Sort in Reverse Order.
+        List reverseOrder = Arrays.asList(arr).stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        System.out.println(reverseOrder);
+
+        // Sort in Natural Ordering.
+        List ans = Arrays.stream(arr).sorted().collect(Collectors.toList());
+        System.out.println(ans);
+    }
+}
--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Q1. Seprate Odd and Even from the given Array using Java Steams.
 
 Q2. Print the Fequency of each character in String.
 
+Q3. Sort the List in Reverse Order.
+
 ```
 
 # Query Using Java 8 Steams
